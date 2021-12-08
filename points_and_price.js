9f92eb42a994049a694b77058d9f13b28eac61c1
kw-8 <kwu11235813@gmail.com>
2021-12-08 14:36:06 -0800

    Convert points and price to numbers

--- points_and_price.js
+++ points_and_price.js
@@ -10,15 +10,15 @@ mongoose
 // convert all price and points values to integers
 Wine.find()
   .then(wines => {
-    console.log(parseInt(wine.points))
-    // console.log(parseInt(wine.price))
-    // wines.forEach(wine => {
-    //   // console.log('outside')
-    //   if (typeof wine.tagIndex[0] === 'string') {
-    //     console.log('inside')
-    //     wine.points = parseInt(wine.points)
-    //     wine.price = parseInt(wine.price)
-    //     // wine.save()
-    //   }
-    // })
+    wines.forEach(wine => {
+      console.log(wine.id)
+      wine.points = parseInt(wine.points)
+      wine.price = parseInt(wine.price)
+      wine.save()
+    })
   })
+
+// console.log(wine)
+// wine.points = parseInt(wine.points)
+// wine.price = parseInt(wine.price)
+// wine.save()
