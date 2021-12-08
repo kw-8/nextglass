cca54a37cc9ccafa2ecdbfa0d3e845a8d4d6b835
David Chan <dchaan1@gmail.com>
2021-12-08 13:50:04 -0800

    Start collections frontend files

---
+++ frontend/src/actions/collection_actions.js
@@ -0,0 +1,45 @@
+import * as APIUtil from "./../util/collection_api_util"
+
+export const RECEIVE_COLLECTIONS = "RECEIVE_COLLECTIONS";
+export const RECEIVE_COLLECTION = "RECEIVE_COLLECTION";
+export const REMOVE_COLLECTION = "REMOVE_COLLECTION";
+
+export const receiveCollections = collections => ({
+  type: RECEIVE_COLLECTIONS,
+  collections
+})
+
+export const receiveCollection = collection => ({
+  type: RECEIVE_COLLECTION,
+  collection
+})
+
+export const removeCollection = collectionId => ({
+  type: REMOVE_COLLECTION,
+  collectionId
+})
+
+export const getCollections = () => dispatch => (
+  APIUtil.getCollections()
+    .then(collections => dispatch(receiveCollections(collections)))
+)
+
+export const getCollection = collectionId => dispatch => (
+  APIUtil.getCollection(collectionId)
+    .then(collection => dispatch(receiveCollection(collection)))
+)
+
+export const createCollection = collection => dispatch => (
+  APIUtil.createCollection(collection)
+    .then(collection => dispatch(receiveCollection(collection)))
+)
+
+export const updateCollection = collection => dispatch => (
+  APIUtil.updateCollection(collection)
+    .then(collection => dispatch(receiveCollection(collection)))
+)
+
+export const deleteCollection = collectionId => dispatch => (
+  APIUtil.deleteCollection(collectionId)
+    .then(collectionId => dispatch(removeCollection(collectionId)))
+)
---
+++ frontend/src/util/collections_api_util.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+// get all collections
+export const getCollections = () => {
+  return axios.get('/api/collections')
+};
+
+// get collection by id
+export const getCollection = collectionId => {
+  return axios.get(`/api/collections/${collectionId}`)
+};
+
+// get user's collections
+// export const getUserCollections = userId => {
+//   return axios.get(`/api/collections/users/${userId}`)
+// }
+
+// create new collection
+export const createCollection = collection => {
+  return axios.post('/api/collections/', collection)
+}
+
+// update collection
+export const updateCollection = collection => {
+  return axios.patch(`/api/collections/${collection.id}`, collection)
+}
+
+export const deleteCollection = collectionId => {
+  return axios.delete(`/api/collections/${collectionId}`)
+}
+
