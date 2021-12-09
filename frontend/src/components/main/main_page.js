0293279d9c2618cf3ad4f71118322dd63fd738ab
MalachiCoberley <malachicoberley@gmail.com>
2021-12-09 12:24:30 -0700

    merge with main

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 class MainPage extends React.Component {
 
   componentDidMount() {
+    this.props.getCollections()
     console.log(this.props)
   }
 
@@ -17,6 +18,7 @@ class MainPage extends React.Component {
         </div>
         <div className="home-page-index-link">
           <Link to="/wines">Find a wine</Link>
+          <Link to="/collections">view your collections</Link>
         </div>
       </div>
     );
--- frontend/src/components/main/main_page_container.js
+++ frontend/src/components/main/main_page_container.js
@@ -1,8 +1,13 @@
 import { connect } from 'react-redux';
 import MainPage from './main_page';
+import {getCollections} from '../../actions/collection_actions'
 
 const mSTP = state => ({
-  currentUser: state.session.user
+  currentUser: state.session.user,
+  collections: Object.values(state.collections)
+})
+const mDTP = dispatch => ({
+  getCollections: () => dispatch(getCollections())
 })
 
-export default connect(mSTP)(MainPage)
+export default connect(mSTP, mDTP)(MainPage)
