09dd96f0a05549721ffb36bcb55db713461aeade
kw-8 <kwu11235813@gmail.com>
2021-12-09 10:59:42 -0800

    Resolved request issue. Collection query now has nested 'collection' and 'suggestions'

--- routes/api/collections.js
+++ routes/api/collections.js
@@ -18,47 +18,44 @@ router.get("/", passport.authenticate("jwt", { session: false} ), (req, res) => 
 router.get("/:id", passport.authenticate("jwt", { session: false }), (req, res) => {
   const collection = Collection
     .findById(req.params.id)
-    .then(collection => (res.json(collection)))
-    .catch(err => res.status(400).json(err))
+    .then(collection => {
+      let wines = collection.wines
+      // for each tag on each wine, add to tag counter in tags
+      let tags = {};
+      wines.forEach(wineId => {
+        Wine.find(wineId).then(wine => {
+          // if tagId exists in tags, count up, o/w set to 1
+          wtags.forEach(tagId =>
+            tags[tagId] = tags[tagId] ? tags[tagId] + 1 : 1
+          )
+        })
+      });
 
-  // let wines = req.params.wines
-  // // for each tag on each wine, add to tag counter in tags
-  // let tags = {};
-  // wines.forEach(wineId => {
-  //   Wines.find(wineId).then(wine => {
-  //     // if tagId exists in tags, count up, o/w set to 1
-  //     wtags.forEach(tagId =>
-  //       tags[tagId] = tags[tagId] ? tags[tagId] + 1 : 1
-  //     )
-  //   })
-  // });
-  // // sort tags by counts
-  // let entries = Object.entries(tags)
-  // let sortedTags = entries.sort((a,b) => a[1] > b[1]).map(tag => tag[0])
-  // while (sortedTags.length < 4) {
-  //   sortedTags.push(Math.floor(Math.random() * 56) + 1)
-  // }
+      // sort tags by counts
+      let entries = Object.entries(tags)
+      let sortedTags = entries.sort((a,b) => a[1] > b[1]).map(tag => tag[0])
+      while (sortedTags.length < 4) {
+        sortedTags.push(Math.floor(Math.random() * 56) + 1)
+      }
 
-  // let primary_tag = sortedTags.slice(0, 1)
-  // let other_tags = sortedTags.slice(1, 4)
-  // let min_price = 15 * 0.6
-  // let max_price = 15 * 1.8
+      let primary_tag = sortedTags.slice(0, 1)
+      let other_tags = sortedTags.slice(1, 4)
+      // let min_price = 15 * 0.6
+      // let max_price = 15 * 1.8
 
-  // const suggestions = Wines
-  //   .find({
-  //     _id: { $nin: wines },
-  //     tagIndex: { $elemMatch: { $in: primary_tag } },
-  //     tagIndex: { $elemMatch: { $in: other_tags } },
-  //     price: { $gte: min_price },
-  //     price: { $lte: max_price },
-  //     points: { $gte: 1 },
-  //     points: { $lte: 100 }
-  //   }).limit(10)
-  //   .then(suggestions => (res.json(suggestions)))
-  //   .catch(err => res.status(400).json(err))
-  
-  // res.json({ collection, suggestions })
-  
+      return Wine.find({
+        _id: { $nin: wines },
+        tagIndex: { $elemMatch: { $in: primary_tag } },
+        tagIndex: { $elemMatch: { $in: other_tags } },
+        // price: { $gte: min_price },
+        // price: { $lte: max_price },
+        // points: { $gte: 1 },
+        // points: { $lte: 100 }
+      }).limit(10)
+        .then(suggestions => (res.json({ collection, suggestions })))
+        .catch(err => res.status(400).json(err))  
+    })
+    .catch(err => res.status(400).json(err))  
 });
 
 // create new collection 
