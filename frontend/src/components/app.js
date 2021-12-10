f0551778f0c1e4b857bf64a4ebb721a614b92d8f
kw-8 <kwu11235813@gmail.com>
2021-12-09 18:22:20 -0800

    Test

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -11,8 +11,8 @@ import CollectionIndexItemContainer from './collection/collection_container'
 
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
+  App.use(express.static('frontend/build'));
+  App.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
