8f3abacd256ba53b0a3146705c9fdafaa3a1c8c5
kw-8 <kwu11235813@gmail.com>
2021-12-09 18:03:50 -0800

    Add script to reduce builds

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -9,6 +9,14 @@ import Splash from './splash/splash';
 import WineIndexContainer from './wine_index/wine_index_container'
 import CollectionIndexItemContainer from './collection/collection_container'
 
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 const App = () => (
   <div>
     <NavBarContainer />
