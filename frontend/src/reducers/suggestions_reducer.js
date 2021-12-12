c06918550181f55bb864e3b678a5031d812d56ce
kw-8 <kwu11235813@gmail.com>
2021-12-12 14:58:11 -0800

    Last state file

--- frontend/src/reducers/suggestions_reducer.js
+++ frontend/src/reducers/suggestions_reducer.js
@@ -4,7 +4,7 @@ const SuggestionReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_COLLECTION:
-      return Object.assign({}, state, { suggestions: action.collection.data.suggestions });
+      return Object.assign({}, state, { [action.collectionId]: action.collection.data.suggestions });
     default:
       return state;
   }
