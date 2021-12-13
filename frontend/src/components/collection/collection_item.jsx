623f423e9ffde877f5632178a895f86626f4a810
kw-8 <kwu11235813@gmail.com>
2021-12-13 15:57:34 -0800

    Remove console.log

--- frontend/src/components/collection/collection_item.jsx
+++ frontend/src/components/collection/collection_item.jsx
@@ -7,7 +7,6 @@ class CollectionItem extends React.Component {
     // debugger
     if (collections[collectionId]) {
       collections[collectionId].wines.forEach(wineId => {
-        console.log(wineId)
         this.props.fetchOneWine(wineId)
       })
       
@@ -16,7 +15,7 @@ class CollectionItem extends React.Component {
       this.props.getCollection(collectionId).then(res => {
         // console.log(res.collection.data.collection.wines, collectionId, this.props)
         res.collection.data.collection.wines.forEach(wineId => {
-          console.log(wineId)
+          // console.log(wineId)
           this.props.fetchOneWine(wineId)
         })
       })
