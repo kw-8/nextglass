bb4c4340392c2a7f0d2c3198cad752438e8e7fc8
MalachiCoberley <malachicoberley@gmail.com>
2021-12-14 15:33:47 -0700

    Link Tags work. Collections Show all wines in state still

--- frontend/src/components/collection/collection.jsx
+++ frontend/src/components/collection/collection.jsx
@@ -10,6 +10,14 @@ class Collection extends React.Component {
   }
 
   render() {
+    //Creates an array of all wines in the current collection
+
+    // let wineArray = []
+    // for (let i = 0; i < this.props.collections.length; i++) {
+    //   if (this.props.collections[i]._id === this.props.match.params.collectionId) {
+    //     wineArray = [...this.props.collections[i].wines]
+    //   }
+    // }
     return(
       <div>
         <CollectionItemContainer 
