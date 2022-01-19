b0d3fe707f3a5a8d8c232d5b48b390776ae67d8b
kw-8 <kwu11235813@gmail.com>
2022-01-19 12:29:48 -0800

    Update pictures

--- frontend/src/components/about/images.jsx
+++ frontend/src/components/about/images.jsx
@@ -16,7 +16,16 @@ const ImageCredit = () => {
     '../bottles_3.jpg',
     '../bottles_4.jpg',
     '../barrels.jpg',
-    '../drinks.jpg'
+    '../drinks.jpg',
+
+    '../light-red.jpeg',
+    '../medium-red.jpg',
+    '../dark-red.jpg',
+    '../wine-variety.jpg',
+    '../dessert-wine.jpg',
+    '../champagne.jpg',
+    '../generic-red.jpg',
+    '../generic-white.jpg'
   ]
   const links = [
     'https://www.flickr.com/photos/calliope/105360353',
@@ -33,7 +42,16 @@ const ImageCredit = () => {
     'https://www.flickr.com/photos/infomastern/28077933944',
     'https://www.flickr.com/photos/nizamosman/4310990993',
     'https://www.flickr.com/photos/futureshape/5224997498',
-    'https://www.flickr.com/photos/haynes/1003014064'
+    'https://www.flickr.com/photos/haynes/1003014064',
+
+    'https://www.brightcellars.com/blog/wp-content/uploads/2020/06/Red-wine-summer-pool-1000x600.jpg',
+    'https://stanneswinery.com.au/the-history-of-merlot-red-wine/',
+    'https://www.flickr.com/photos/quinnanya/5901472786',
+    'https://www.flickr.com/photos/shadesta/7276532472',
+    'https://www.flickr.com/photos/149561324@N03/27819129658',
+    'https://www.flickr.com/photos/zeeyolqpictures/45405409804',
+    'https://www.flickr.com/photos/30997751@N07/28695241343',
+    'https://www.flickr.com/photos/njj001/10598663226'
   ]
   return (
     <div className='image-credit-container'>
@@ -49,7 +67,6 @@ const ImageCredit = () => {
           )
         }
       </div>
-      <p>Pictures we have used are from Flickr under commercial license.</p>
     </div>
   )
 }
